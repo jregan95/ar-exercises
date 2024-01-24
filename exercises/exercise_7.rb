05dcb1ff54937ea81f2f9abcb45380aa6b82fcf3
Jill Regan <jillian.jnr@hotmail.com>
2024-01-24 15:54:20 -0500

    Complete: exercise 7, printing error message.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,8 +6,16 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
+
+
 puts "Exercise 7"
 puts "----------"
 
+new_name = gets.chomp
+
+store = Store.create(name: new_name)
+
+puts store.errors.full_messages
+
 # Your code goes here ...
-#bundle exec ruby exercises/exercise_2.rb
+#bundle exec ruby exercises/exercise_7.rb
