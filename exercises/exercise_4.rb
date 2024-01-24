da3a8d7c52c8d7fe1341878bbee5f5310703687c
Jill Regan <jillian.jnr@hotmail.com>
2024-01-24 15:08:43 -0500

    Completed: exercise 4. Search using where

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,8 +3,18 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
+
 puts "Exercise 4"
 puts "----------"
 
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true);
+Store.create(name: "Whistler", annual_revenue: 190000, mens_apparel: true, womens_apparel: false);
+Store.create(name: "Yaletown", annual_revenue: 43000, mens_apparel: true, womens_apparel: true);
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each { |x| puts "#{x.name} #{x.annual_revenue}"}
+
+@womens_apparel = Store.where('annual_revenue < ? AND womens_apparel = ?', 1000000, true)
+@womens_apparel.each { |x| puts "#{x.name} #{x.annual_revenue}"}
+
 # Your code goes here ...
-#bundle exec ruby exercises/exercise_2.rb
